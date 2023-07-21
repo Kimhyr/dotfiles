c77669fc768485c89c0413705a63ebe77224f956
King Emhyr <kinglanchester@gmail.com>
2023-07-21 01:04:43 -0400

    .

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+export EDITOR=helix
+export QT_QPA_PLATFORM=wayland
+
+set fish_greeting
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias hx=helix
---
+++ .config/sway/status.fish
@@ -0,0 +1,4 @@
+#!/bin/env fish
+
+while true
+end
