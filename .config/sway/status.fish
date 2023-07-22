e2f77581c61b56821432941c728e83760b5657dc
King Emhyr <kinglanchester@gmail.com>
2023-07-21 20:59:11 -0400

    .

--- .config/sway/status.fish
+++ .config/sway/status.fish
@@ -1,4 +1,16 @@
 #!/bin/env fish
 
+set battery (upower -e | grep battery)
+
+function get_power -a info prop
+    echo "$(echo $info | grep $prop | sed "s/\\s*$prop:\\s*//g")"
+end
+
 while true
+    set date "date: $(date +'%x %X')"
+    set pinfo "$(upower -i $battery)"
+    set power "power: $(get_power $pinfo percentage) $(get_power $pinfo state)"
+    set brightness "brightness: $(math "$(brightnessctl g) / $(brightnessctl m) * 100")%"
+    echo "$power | $brightness | $date"
+    sleep 1
 end
